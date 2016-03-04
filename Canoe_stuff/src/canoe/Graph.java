d42fa5d5e7236a516a3db4760b65ac4045404963
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-03-04 14:58:49 -0800

    Built dijkstra's

--- Canoe_stuff/src/canoe/Graph.java
+++ Canoe_stuff/src/canoe/Graph.java
@@ -1,38 +1,84 @@
 package canoe;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Graph {
 	private Node startNode;
 	private LinkedList<Node> myNodes;
-	
+
 	public Graph(Node firstNode){
 		startNode = firstNode;
 		myNodes = new LinkedList<Node>();
 	}
-	
+
 	public void addNode(Node inputNode){
 		myNodes.add(inputNode);
 	}
-	
+
 	public LinkedList<Node> getNodes(){
 		return myNodes;
 	}
-	
+
 	public int size(){
 		return myNodes.size();
 	}
-	
+
 	public Node getFirst(){
 		return myNodes.getFirst();
 	}
-	
-	
-//	public ArrayList<Node> BFS(){
-//		
-//	}
-	
-	
+
+
+	public ArrayList<Node> BFS(){
+		ArrayList<Node> toReturn = new ArrayList<Node>();
+		ArrayList<Node> visited = new ArrayList<Node>();
+
+		Queue<Node> q = new PriorityQueue<Node>();
+
+		q.add(myNodes.getFirst());
+		while(!q.isEmpty()) {
+			Node front = q.poll();
+			if(!visited.contains(front)) {
+				visited.add(front);
+				for(int i = 0; i< front.getEdges().size(); i++){ 
+					if(!visited.contains(front.getEdges().get(i))){
+						Node current = front.getEdges().get(i).getNextNode();
+						q.add(current);
+					}
+				}
+			}
+		}
+		return toReturn;
+	}
+
+	public int[] Dijkstra(Node source, int[][] inputMatrix){
+		int[] toReturn = new int[myNodes.size()-1];
+		source.setMinDistance(inputMatrix[0][1]);
+		for(int i = 1; i< myNodes.size(); i++){
+			toReturn[i] = 99999;
+		}
+		toReturn[0] = source.getMinDistance();
+		PriorityQueue<Node> nodes = new PriorityQueue<Node>();
+		nodes.add(source);
+		while(!nodes.isEmpty()){
+			Node current = nodes.poll();
+
+			for(Edge e: current.getEdges()){
+				Node n = e.getNextNode();
+				int weight = e.getWeight();
+				int distance = current.getMinDistance() + weight;
+				if(distance < n.getMinDistance()){
+					n.setMinDistance(distance);
+					toReturn[n.getElement()-1] = n.getMinDistance();
+				}
+				
+			}
+		}
+		return toReturn;
+	}
+
+
 	public void printGraph(){
 		LinkedList<Node> nodes = getNodes();
 		LinkedList<Edge> edges;
@@ -48,6 +94,6 @@ public class Graph {
 			}
 		}
 	}
-	
+
 }
 
